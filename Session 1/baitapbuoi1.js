8a9eddd537fd23466598fd1e9d4307973124ca60
Hoang Mai <hmaivu02@gmail.com>
2021-03-12 21:39:53 +0700

    Sua trong index

---
+++ Session 1/baitapbuoi1.js
@@ -0,0 +1,47 @@
+// let message = 'Coding might not be easy, but still great';
+// console.log (message);
+// let studentCount = 21; 
+// console.log (studentCount);
+// console.log (message.toLowerCase());
+// console.log (studentCount + 1);
+
+// let niceMessage = 'Remember to stay hydrated!';
+// alert (niceMessage);
+
+// let askUser = prompt ('Hey, what can I call you?');
+// alert ('Oh, hi ' + askUser);
+// alert ('You are really cute, ' + askUser);
+
+// let askFirstName = prompt ('Hi, what is your first name?');
+// let askLastName = prompt ('And your last name?');
+// alert ('Hello there, ' + askLastName + ' ' + askFirstName + '!');
+
+// let sideLength = prompt ('What is the side length of the square in meters?');
+// alert (`The area of the square is ${sideLength * sideLength} square meters.`);
+
+// let radius = prompt ('What is the radius of the circle in meters?')
+// alert (`The are of the cirlce is ${2 * Math.PI * radius} square meters.`)
+
+// let celsius = prompt ('Enter the temperature in Celsius.');
+// let farenheit = celsius * 1.8 + 32;
+// alert (celsius + ' (C) = ' + `${celsius * 1.8 + 32}` + ' (F).');
+
+//for (let l=0; l<5; l++) {let askYou = prompt ('What is your name?'); console.log (askYou);}
+//for (let x=5; x<=70; x+=5) {console.log (x)};
+
+let weight = prompt ('How much do you weigh in kilograms?');
+let height = prompt ('How tall are you in meters?');
+let BMI = weight/(height*height);
+if (BMI < 18.5) {alert ('You are underweight.');} 
+else if (BMI < 25) {alert ('Your weight is normal.');}
+else if (BMI < 30) {alert ('You are a little overweight.');}
+else if (BMI < 35) {alert ('You have level 1 obesity.');}
+
+// let a = prompt ('Enter the value of a');
+// let b = prompt ('Enter the value of b');
+// let c = prompt ('Enter the value of c');
+// let delta = b^2-4*a*c;
+// if (delta > 0) {alert ('Your quadratic equation has 2 different roots.');}
+// if (delta <0) {alert ('Your quadratic equation has no root.');}
+// else {alert ('Your quadratic equation has 1 root.');}
+
