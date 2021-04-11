ca4a30fff74ac82d8dcfb77d0830d66fe55e0d00
Hoang Mai <hmaivu02@gmail.com>
2021-04-11 14:12:24 +0700

    project

---
+++ project/project.js
@@ -0,0 +1,377 @@
+let songDataAll = [
+    {
+        name: "Cigarette Daydreams",
+        author: "Cage The Elephant",
+        singer: "Cage The Elephant",
+        genre: "Rock",
+        lyrics: `Did you stand there all alone?
+        Oh I cannot explain what's going down
+        I can see you standing next to me
+        In and out somewhere else right now
+        You sigh, look away
+        I can see it clear as day
+        Close your eyes, so afraid
+        Hide behind that baby face
+        Do do do do do do
+
+        You can drive all night
+        Looking for answers in the pouring rain
+        You wanna find peace of mind
+        Looking for the answer
+
+        Funny how it seems like yesterday
+        As I recall you were looking out of place
+        Gathered up your things and slipped away
+        No time at all I followed you into the hall
+        Cigarette daydream
+        You were only seventeen
+        Soft speak with a mean streak
+        Nearly brought me to my knees...`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/Fid47nhpLXAo`
+    },
+    {
+        name: "Pride And Joy",
+        author: "Stevie Ray Vaughan",
+        singer: "Stevie Ray Vaughan",
+        genre: "Rock",
+        lyrics: `Well you've heard about love givin' sight to the blind
+        My baby's lovin' cause the sun to shine
+        She's my sweet little thang...
+        She's my pride and joy
+        She's my sweet little baby...
+        I'm her little lover boy
+
+        Yeah I love my baby...
+        Heart and soul
+        Love like ours won't never grow old
+        She's my sweet little thang...
+        She's my pride and joy
+        She's my sweet little baby...
+        I'm her little lover boy
+
+        Yeah I love my lady...
+        She's long and lean
+        You mess with her...
+        You'll see a man get mean
+        She's my sweet little thang...
+        She's my pride and joy
+        She's my sweet little baby...
+        I'm her little lover boy
+
+        Well I love my baby...
+        Like the finest wine
+        Stick with her until the end of time
+        She's my sweet little thang...
+        She's my pride and joy
+        She's my sweet little baby...
+        I'm her little lover boy
+
+        Yeah I love my baby...
+        Heart and soul
+        Love like ours won't never grow old
+        She's my sweet little thang...
+        She's my pride and joy
+        She's my sweet little baby...
+        I'm her little lover boy`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/sKMJDIOPEOYL`
+    },
+    {
+        name: "Killer Queen",
+        author: "Freedie Mercury",
+        singer: "Queen",
+        genre: "Rock",
+        lyrics: `She keeps her Moet et Chandon
+        In her pretty cabinet
+        "Let them eat cake", she says
+        Just like Marie Antoinette
+        A built-in remedy
+        For Khrushchev and Kennedy
+        At anytime an invitation
+        You can't decline
+
+        Caviar and cigarettes
+        Well versed in etiquette
+        Extraordinarily nice
+
+        She's a Killer Queen
+        Gunpowder, gelatine
+        Dynamite with a laser beam
+        Guaranteed to blow your mind
+        Anytime
+
+        Recommended at the price
+        Insatiable an appetite
+        Wanna try?
+
+        To avoid complications
+        She never kept the same address
+        In conversation
+        She spoke just like a baroness
+        Met a man from China
+        Went down to Geisha Minah
+        Then again incidentally
+        If you're that way inclined
+
+        Perfume came naturally from Paris (naturally)
+        For cars she couldn't care less
+        Fastidious and precise
+
+        Drop of a hat she's as willing as
+        Playful as a pussy cat
+        Then momentarily out of action
+        Temporarily out of gas
+        To absolutely drive you wild, wild
+        She's all out to get you`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/cFpN4CVqnTBW`
+    },
+    {
+        name: "Nếu Ngày Mai Tôi Không Trở Về",
+        author: "The Cassette",
+        singer: "The Cassette",
+        genre: "Rock",
+        lyrics: `Nếu như ngày mai tôi không trở về
+        Xin đừng đợi thêm nữa
+        Mùa thu đến sao đi quá vội
+        Tôi mãi xa người thật rồi
+        
+        Mười tám lá thư tay tôi vẫn không thể nào viết hết
+        Bài hát vẫn chưa bắt đầu, tối biết rằng người muốn nghe
+        Từng giấc chiêm bao tôi vãn không thể nào nhớ hết
+        Để gió đưa lời tôi ngọt ngào đi đến nơi người
+        
+        Ngày vẫn trôi vẫn tôi một mình một góc thôi
+        Giờ đây tôi muốn nói...
+        
+        Nếu như ngày mai tôi không trở về
+        Xin đừng đợi thêm nữa
+        Ngày hoa rớt trên cách đồng tôi biết rằng tôi đã yêu
+        Nếu như ngày mai tôi không trở về
+        Xin đừng đợi thêm nữa
+        Mùa thu đến sao đi quá vội tôi mãi xa người thật rồi
+        
+        Ngày vẫn trôi vẫn tôi một mình một góc thôi
+        Giờ đây tôi muốn nói...`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/707CMVT1ysrV`
+    },
+    {
+        name: "Cơn Mưa Tháng 5",
+        author: "Bức Tường",
+        singer: "Tùng Dương, Trần Lập",
+        genre: "Rock",
+        lyrics: `Ngủ vùi cùng ký ức về trong giấc mơ luôn gọi tên
+        Đôi khi tôi muốn cố quên đi, nhưng xoá sao ngàn vết dấu
+        Thèm trở lại nơi đã từng, ngày đó khi ta gần bên
+        Rồi một ngày em đã quay lưng,
+        Trong cơn mưa tháng 5...
+
+        Dấu chân lạc bước trên phố quen, nơi cơn mơ tôi đi vào.
+        Để tìm lại đúng nơi trước khi ào ạt cơn mưa tới
+        Muốn sửa lại phút giây đã sai cho em sẽ quay ngoảnh lại
+        Bài tình ca tháng 5 dành cho em
+
+        Còn ngân vang hay tan biến theo mưa ướt nhoà...
+        Rồi từng ngày vẫn như thường, nắng có xua bớt muộn phiền
+        Những ước muốn rồi sẽ phai dần khi giấc mơ qua một đêm
+        Trong cơn mưa như bất tận, vẫn thấy đâu đây một tiếng cười
+        Ánh mắt ấy vẫn chứa chan dịu dàng
+        Khi giấc mơ qua...
+
+        Dấu chân lạc bước trên phố quen, nơi cơn mơ tôi đi vào
+        Để tìm về đúng nơi trước khi ào ạt cơn mưa tới
+        Muốn sửa lại phút giây đã sai cho em sẽ quay ngoảnh lại
+        Bài tình ca tháng 5 dành cho em
+        Vội tan trong mưa...`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/3LNaIJ3GxcnE`
+    },
+    {
+        name: "Yêu",
+        author: "7UPPERCUTS",
+        singer: "7UPPERCUTS",
+        genre: "Rock",
+        lyrics: `Cho tới khi nào
+        Thì ta được chung lối về
+        Anh chỉ muốn có em bên anh trên con đường dài lê thê
+        Cùng ăn sáng, nghe đài
+        Mỗi sớm mai sau khi ngáp dài
+        Là một ngày mới, có em, và anh
+        
+        Anh không biết
+        Là em
+        Đã đọc một mớ tin nhắn hôm qua anh viết
+        Gửi em
+        Cái gì mà sống không có em
+        Anh không thiết
+        Là thiệt
+        Nhưng nếu em phiền thì thôi mình coi như anh bị điên
+        Anh không chắc
+        Là em
+        Đã đọc vì sáng nay mesenger không nhắc
+        Rồi quên
+        Ra đường ngồi đếm xe máy
+        Ba đồng một mớ muộn phiền
+        Ai nói anh khùng thì thôi, vì anh chỉ cần được
+        Yêu không yêu không yêu
+        Yêu không yêu không yêu
+        Yêu không yêu không yêu
+        Không
+        Yêu không yêu không yêu
+        Yêu không yêu không yêu
+        Yêu
+        
+        Anh không biết
+        Ngày sau
+        Đi về còn có ai đó mong chờ thang máy
+        Cùng nhau
+        Lên lầu mình có con chó con mèo
+        Băng cốc
+        Mình đi
+        Tháng 2 lần
+        Lần 2 qua chatuchak thì sao`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/KmFfZseooeHo`
+    },
+    {
+        name: "Won't Go Home Without You",
+        author: "Maroon 5",
+        singer: "Adam Levine, Maroon 5",
+        genre: "Rock",
+        lyrics: `I asked her to stay
+        But she wouldn't listen
+        And she left before I had the chance to say, oh
+        The words that would mend
+        The things that were broken
+        But now it's far too late; she's gone away
+
+        Every night you cry yourself to sleep
+        Thinking: "Why does this happen to me?
+        Why does every moment have to be so hard?"
+        Hard to believe that
+
+        It's not over tonight
+        Just give me one more chance to make it right
+        I may not make it through the night
+        I won't go home without you
+
+        The taste of her breath
+        I'll never get over
+        The noises that you made kept me awake
+        Oh
+        The weight of the things that remained unspoken
+        Built up so much it crushed us everyday
+        
+        Of all the things I felt but never really shown
+        Perhaps the worst is that I ever let you go
+        I should not ever let you go, oh, oh, oh`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/rTkV54XgVuNI`
+    },
+    {
+        name: "Believer",
+        author: "Imagine Dragons",
+        singer: "Imagine Dragons",
+        genre: "Rock",
+        lyrics: `First things first
+        I'ma say all the words inside my head
+        I'm fired up and tired of the way that things have been, oh-ooh
+        The way that things have been, oh-ooh
+
+        Second thing second
+        Don't you tell me what you think that I can be
+        I'm the one at the sail, I'm the master of my sea, oh-ooh
+        The master of my sea, oh-ooh
+
+        I was broken from a young age
+        Taking my sulking to the masses
+        Writing my poems for the few
+        That looked at me, took to me, shook to me, feeling me
+        Singing from heartache from the pain
+        Take up my message from the veins
+        Speaking my lesson from the brain
+        Seeing the beauty through the
+
+        Pain!
+        You made me a, you made me a believer, believer
+        Pain!
+        You break me down, you build me up, believer, believer
+        Believer, believer
+        Believer, believer
+        Believer, believer
+
+        Third things third
+        Send a prayer to the ones up above
+        All the hate that you've heard has turned your spirit to a dove, oh-ooh
+        Your spirit up above, oh-ooh
+
+        I was choking in the crowd
+        Building my rain up in the cloud
+        Falling like ashes to the ground
+        Hoping my feelings, they would drown
+        But they never did, ever lived, ebbing and flowing
+        Inhibited, limited
+        'Til it broke open and rained down
+        It rained down, like`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/XVuxq5urS59m`
+    },
+    {
+        name: "Have You Ever Seen The Rain",
+        author: "John Fogerty",
+        singer: "Sawyer Fredericks, Noelle Bybee",
+        genre: "Rock",
+        lyrics: `Someone told me long ago
+        There's a calm before the storm
+        I know
+        It's been comin' for some time
+
+        When it's over, so they say
+        It'll rain a sunny day
+        I know
+        Shinin' down like water
+
+        I want to know, have you ever seen the rain?
+        I want to know, have you ever seen the rain
+        Comin' down on a sunny day?
+
+        Yesterday, and days before
+        Sun is cold and rain is hard
+        I know
+        Been that way for all my time
+
+        'Til forever, on it goes
+        Through the circle, fast and slow
+        I know
+        It can't stop, I wonder
+
+        I want to know, have you ever seen the rain?
+        I want to know, have you ever seen the rain
+        Comin' down on a sunny day?`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/l75mPODjDLHZ`
+    },
+    {
+        name: "K.",
+        author: "Cigarettes After Sex",
+        singer: "Cigarettes After Sex",
+        genre: "Rock",
+        lyrics: `I remember when I first noticed that you liked me back
+        We were sitting down in a restaurant waiting for the check
+        We had made love earlier that day with no strings attached
+        But I could tell that something had changed how you looked at me then
+        
+        Kristen, come right back
+        I've been waiting for you to slip back in bed
+        When you light the candle
+        
+        And on the Lower East Side you're dancing with me now
+        And I'm taking pictures of you with flowers on the wall
+        Think I like you best when you're dressed in black from head to toe
+        Think I like you best when you're just with me
+        And no one else
+        
+        Kristen, come right back
+        I've been waiting for you to slip back in bed
+        When you light the candle
+        
+        And I'm kissing you lying in my room
+        Holding you until you fall asleep
+        And it's just as good as I knew it would be
+        Stay with me I don't want you to leave`,
+        iframeUrl: `https://www.nhaccuatui.com/mh/normal/QC8tVw3tMrwM`
+    }
+]
